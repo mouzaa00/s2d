f9bec037f6bf6518bd837af4bd5c4d0e64ac7d0e
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2026-06-10 10:06:26 +0100

    add clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,13 @@ import "./globals.css";
 import { cn } from "~/lib/utils";
 import { ThemeProvider } from "~/theme/provider";
 import { Toaster } from "~/components/ui/sonner";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -42,8 +49,23 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
-          <Toaster />
+          <ClerkProvider>
+            <header className="flex justify-end items-center p-4 gap-4 h-16">
+              <Show when="signed-out">
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </Show>
+              <Show when="signed-in">
+                <UserButton />
+              </Show>
+            </header>
+            {children}
+            <Toaster />
+          </ClerkProvider>
         </ThemeProvider>
       </body>
     </html>
