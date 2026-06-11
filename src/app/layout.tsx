5e39b1e938e2cce0f382b72ad626f0a7243140fc
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2026-06-11 22:52:33 +0100

    setup convex with clerk and mock queries

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import {
   SignUpButton,
   UserButton,
 } from "@clerk/nextjs";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -50,21 +51,23 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <ClerkProvider>
-            <header className="flex justify-end items-center p-4 gap-4 h-16">
-              <Show when="signed-out">
-                <SignInButton />
-                <SignUpButton>
-                  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                    Sign Up
-                  </button>
-                </SignUpButton>
-              </Show>
-              <Show when="signed-in">
-                <UserButton />
-              </Show>
-            </header>
-            {children}
-            <Toaster />
+            <ConvexClientProvider>
+              <header className="flex justify-end items-center p-4 gap-4 h-16">
+                <Show when="signed-out">
+                  <SignInButton />
+                  <SignUpButton>
+                    <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                      Sign Up
+                    </button>
+                  </SignUpButton>
+                </Show>
+                <Show when="signed-in">
+                  <UserButton />
+                </Show>
+              </header>
+              {children}
+              <Toaster />
+            </ConvexClientProvider>
           </ClerkProvider>
         </ThemeProvider>
       </body>
